b9f54da57fd24c12ea043e9719121ab9ec648499
Space-7818 <142069310+HQJ2221@users.noreply.github.com>
2024-06-05 13:47:17 +0800

    Update minOperationsToIncreaseArray.cpp

--- CS208 Algorithm Design and Analysis/Lab 10/minOperationsToIncreaseArray.cpp
+++ CS208 Algorithm Design and Analysis/Lab 10/minOperationsToIncreaseArray.cpp
@@ -19,24 +19,24 @@ inline void write(int x);
  * Output: find the min value changes with min operations to make the array increase
  */
 
-pair<int, int> minOperationsToIncreaseArray(vector<int> &A, const set<int> &set) {
-    vector<vector<pair<int, int>>> dp(A.size(), vector<pair<int, int>>(set.size(), {INT_MAX, INT_MAX}));
-    dp[0][0] = A[0] == *next(set.begin(), 0) ? pair<int, int>{0, 0} : pair<int, int>{1, abs(A[0] - *next(set.begin(), 0))};
-    for (int i = 1; i < set.size(); ++i) {
-        if (A[0] == *next(set.begin(), i)) {
+pair<int, int> minOperationsToIncreaseArray(vector<int> &A, int * arr, int n) {
+    vector<vector<pair<int, int>>> dp(A.size(), vector<pair<int, int>>(n, {INT_MAX, INT_MAX}));
+    dp[0][0] = A[0] == arr[0] ? pair<int, int>{0, 0} : pair<int, int>{1, abs(A[0] - arr[0])};
+    for (int i = 1; i < n; ++i) {
+        if (A[0] == arr[i]) {
             dp[0][i] = {0, 0};
         } else if (dp[0][i - 1].first == 0) {
             dp[0][i] = {0, 0};
         } else {
-            dp[0][i] = {1, abs(A[0] - *next(set.begin(), i))};
+            dp[0][i] = {1, abs(A[0] - arr[i])};
         }
     }
     for (int i = 1; i < A.size(); ++i) {
-        int val = *next(set.begin(), 0);
+        int val = arr[0];
         dp[i][0] = val == A[i] ? dp[i - 1][0] : pair<int, int>{dp[i - 1][0].first + 1,
                                                                dp[i - 1][0].second + abs(A[i] - val)};
-        for (int j = 1; j < set.size(); ++j) {
-            val = *next(set.begin(), j);
+        for (int j = 1; j < n; ++j) {
+            val = arr[j];
             if (A[i] == val) {
                 dp[i][j] = dp[i - 1][j];
             } else {
@@ -52,19 +52,21 @@ pair<int, int> minOperationsToIncreaseArray(vector<int> &A, const set<int> &set)
             }
         }
     }
-    return dp[A.size() - 1][set.size() - 1];
+    return dp[A.size() - 1][n - 1];
 }
 
 int main() {
     // input
     int n = readInt();
     vector<int> A(n);
-    set<int> set;
+    int arr[n];
     for (int i = 0; i < n; ++i) {
         A[i] = readInt() - (i + 1);
-        set.insert(A[i]);
+        arr[i] = A[i];
     }
-    pair<int, int> result = minOperationsToIncreaseArray(A, set);
+    sort(arr, arr + n, less<int>());
+    n = unique(arr, arr + n) - arr;
+    pair<int, int> result = minOperationsToIncreaseArray(A, arr, n);
     write(result.first);
     putchar('\n');
     write(result.second);
@@ -118,4 +120,3 @@ inline void write(long x) {
 inline void write(int x) {
     write((long) x);
 }
-
